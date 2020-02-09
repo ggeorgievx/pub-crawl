0ba7d6717e128672d3e6755aa1a0312ab3f05b44
Georgi Georgiev <georgiwess@gmail.com>
2020-02-09 08:11:43 +0200

    Adjust the Home's height to 100% - 112px, add a link to the create page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,62 +3,90 @@ import Button from '@material-ui/core/Button';
 import Background from '../../assets/images/background.jpg';
 import Tooltip from '@material-ui/core/Tooltip';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
-import Box from '@material-ui/core/Box';
+import { useHistory } from 'react-router-dom';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   home: {
     backgroundImage: `url(${Background})`,
     backgroundPosition: 'center',
     backgroundSize: 'cover',
     position: 'fixed',
     width: '100%',
-    height: 'calc(100% - 120px);',
+    height: 'calc(100% - 112px);',
     textAlign: 'center',
     display: 'flex',
     justifyContent: 'center',
     flexDirection: 'column'
   },
+  blur: {
+    backgroundColor: 'rgba(224, 224, 224, 0.5)',
+    backdropFilter: 'blur(2px)',
+    padding: '50px'
+  },
+  text: {
+    ...theme.typography.h2,
+    color: theme.palette.secondary.main,
+    letterSpacing: '10px',
+    // Prevent the user from selecting the text.
+    userSelect: 'none',
+    msUserSelect: 'none',
+    msTouchSelect: 'none',
+    WebkitUserSelect: 'none',
+    KhtmlUserSelect: 'none',
+    MozUserSelect: 'none',
+    cursor: 'default',
+    '@media (max-width:850px)': {
+      ...theme.typography.h4,
+      letterSpacing: '4px',
+      fontWeight: 300
+    }
+  },
   container: {
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center',
     '@media (max-width:850px)': {
+      alignItems: 'center',
       flexDirection: 'column'
     }
   },
-  tooltip: {
-    fontSize: 14
-  },
   button: {
     margin: '2em'
-  },
-  blur: {
-    backgroundColor: 'rgba(200, 200, 200, 0.5)',
-    backdropFilter: 'blur(5px)',
-    padding: '50px'
   }
-});
+}));
 
 const Home = () => {
   const classes = useStyles();
+  const history = useHistory();
+
+  const guestButtonHandler = () => {
+    history.push('/create');
+  };
 
   return (
     <div className={classes.home}>
       <div className={classes.blur}>
-        <Typography variant="h2" color="secondary">
-          <Box letterSpacing={10}>
-            Design Your Journey
-          </Box>
-        </Typography>
+        <div className={classes.text}>
+          Design Your Journey
+        </div>
         <div className={classes.container}>
-          <Tooltip className={classes.tooltip} title="WITHOUT HISTORY" placement="top">
-            <Button className={classes.button} variant="contained" color="primary">
+          <Tooltip title="WITHOUT HISTORY" placement="top">
+            <Button
+              className={classes.button}
+              variant="contained"
+              color="primary"
+              size="large"
+              onClick={guestButtonHandler}
+            >
               CONTINUE AS GUEST
             </Button>
           </Tooltip>
-          <Tooltip className={classes.tooltip} title="WITH HISTORY" placement="top">
-            <Button className={classes.button} variant="contained" color="primary">
+          <Tooltip title="WITH HISTORY" placement="top">
+            <Button
+              className={classes.button}
+              variant="contained"
+              color="primary"
+              size="large"
+            >
               LOGIN WITH GOOGLE
             </Button>
           </Tooltip>

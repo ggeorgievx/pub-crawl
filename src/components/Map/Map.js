4f813c22c105cb465e1dcf022888024f2f1f5ed5
Georgi Georgiev <georgiwess@gmail.com>
2020-02-13 17:47:10 +0200

    Add the CustomMarker and a DirectionsRenderer to the Maps component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,26 +1,64 @@
 import React, { memo } from 'react';
 import {
+  withScriptjs,
   withGoogleMap,
   GoogleMap,
-  withScriptjs
+  DirectionsRenderer
 } from 'react-google-maps';
 import style from './style';
+import PropTypes from 'prop-types';
+import { forbidExtraProps } from 'airbnb-prop-types';
+import CustomMarker from '../../containers/CustomMarker/CustomMarker';
 
-const Map = () => {
+const Map = (props) => {
   return (
     <GoogleMap
       zoom={12}
-      center={{ lat: 42.698334, lng: 23.319941 }}
+      defaultCenter={{ lat: 42.698334, lng: 23.319941 }}
       options={{
         styles: style,
         mapTypeControl: false,
         fullscreenControl: false,
-        streetViewControl: false
+        streetViewControl: false,
+        clickableIcons: false,
+        minZoom: 5,
+        maxZoom: 20
       }}
-    />
+    >
+      {props.pubMarkersInfo.map((pubMarkerInfo, index) => (
+        <CustomMarker
+          key={index}
+          name={pubMarkerInfo.name}
+          location={pubMarkerInfo.location}
+          index={index}
+          pubMarkersInfoLength={props.pubMarkersInfo.length}
+        />
+      ))}
+      {props.directions && (
+        <DirectionsRenderer
+          directions={props.directions}
+          options={{
+            suppressMarkers: true,
+            polylineOptions: {
+              clickable: false,
+              strokeColor: '#f3004d',
+              strokeOpacity: .3
+            }
+          }}
+        />
+      )}
+    </GoogleMap>
   );
 };
 
-export default memo(withScriptjs(withGoogleMap(Map)), () => {
-  return true;
+Map.propTypes = forbidExtraProps({
+  pubMarkersInfo: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    location: PropTypes.shape({
+      lat: PropTypes.number.isRequired,
+      lng: PropTypes.number.isRequired
+    }).isRequired
+  }).isRequired).isRequired
 });
+
+export default withScriptjs(withGoogleMap(Map));

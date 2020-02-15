bda3ae7d3d78437594920b91c2ccd07583279456
Georgi Georgiev <georgiwess@gmail.com>
2020-02-15 04:48:22 +0200

    Credit Freepik from Flaticon for the icons

--- src/components/Credits/Credits.js
+++ src/components/Credits/Credits.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
 
-const Credits = () => {
-  return (<div>Credits</div>);
+const useStyles = makeStyles({
+  container: {
+    backgroundColor: 'rgb(0, 0, 0)',
+    position: 'fixed',
+    width: '100%',
+    height: 'calc(100% - 112px)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  flaticon: {
+    color: 'rgb(255, 255, 255)'
+  }
+});
+
+const NotFound = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.flaticon}>
+        Icons made by&nbsp;
+        <Link
+          color="secondary"
+          href="https://www.flaticon.com/authors/freepik"
+          target="_blank"
+        >
+          Freepik
+        </Link>
+        &nbsp;from&nbsp;
+        <Link
+          color="secondary"
+          href="https://www.flaticon.com/"
+          target="_blank"
+        >
+          www.flaticon.com
+        </Link>
+      </div>
+    </div>
+  );
 };
 
-export default Credits;
+export default NotFound;
